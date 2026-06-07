37df8a7ba7057fa8a6018becbf669c459dfc20b5
acedep12-sys <acedep12@gmail.com>
2026-06-07 07:39:15 +0330

    Delete ShooterAI_v3 directory

--- ShooterAI_v3/Source/SquadAI/Private/SquadAITuning.cpp
+++
@@ -1,8 +0,0 @@
-// SquadAITuning.cpp
-#include "SquadAITuning.h"
-
-USquadAITuning::USquadAITuning()
-{
-	// Defaults are set in the header via UPROPERTY initializers
-	// Category is set via GetCategoryName() override in the header
-}
